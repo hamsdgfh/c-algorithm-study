fa529c22d852e825851dd632af9b41ae6cd509fe
minseok <hamston@hs.ac.kr>
2025-09-19 22:27:05 +0900

    Create 기능개발.c
    pgs: Level2 기능개발

---
+++ programmers/Level2/기능개발.c
@@ -0,0 +1,9 @@
+/*
+ * Platform : Programmers
+ * Level    : 2
+ * Problem  : 기능개발
+ * Link     : https://school.programmers.co.kr/learn/courses/30/lessons/42586
+ * Idea     : 남은 일수 계산 → 큐/그룹핑
+ * Complex. : O(n)
+ * Note     : 배포 묶음 처리
+ */
