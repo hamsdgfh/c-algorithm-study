3c301d3ac1294e194b3b65d82708d070b57d9c39
minseok <hamston@hs.ac.kr>
2025-09-19 22:24:43 +0900

    Create 1181_단어정렬.c
    boj: 1181 단어정렬 (Silver V)

---
+++ baekjoon/Silver/1181_단어정렬.c
@@ -0,0 +1,11 @@
+/*
+ * Platform : BOJ (백준)
+ * Problem  : 1181 단어 정렬
+ * Tier     : Silver V
+ * Link     : https://www.acmicpc.net/problem/1181
+ * Idea     : 길이 → 사전순 정렬
+ * Complex. : O(n log n)
+ * Note     : 중복 제거
+ */
+#include <stdio.h>
+// 코드 작성...
