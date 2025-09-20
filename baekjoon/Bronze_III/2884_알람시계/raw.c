bc726b94923dd45ec31f189bba0b7f497e0078f1
minseok <hamston@hs.ac.kr>
2025-09-20 15:04:58 +0900

    boj: 2884 알람시계 [raw]

---
+++ baekjoon/Bronze_III/2884_알람시계/raw.c
@@ -0,0 +1,31 @@
+/*
+ * Platform : BOJ (백준)
+ * Problem  : 2884 알람 시계
+ * Tier     : Bronze III
+ * Link     : https://www.acmicpc.net/problem/2884
+ * Idea     : 
+ * Complex. : 
+ * Note     : 
+ */
+
+#include <stdio.h>
+
+int main() {
+    int H, M;
+    int H1, M1;
+    int time;
+    
+    scanf("%d %d", &H, &M);
+
+    time = H * 60 + M - 45;
+
+    if (time < 0)
+        time += 24 * 60;
+
+    H1 = time / 60 % 24;
+    M1 = time % 60;
+
+    printf("%d %d", H1, M1);
+
+    return 0;
+}
