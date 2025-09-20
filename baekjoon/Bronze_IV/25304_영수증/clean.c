872904b0643ecb5b823db4e443c80fec713f3a16
minseok <hamston@hs.ac.kr>
2025-09-20 15:40:19 +0900

    Update clean.c

--- baekjoon/Bronze_IV/25304_영수증/clean.c
+++ baekjoon/Bronze_IV/25304_영수증/clean.c
@@ -1,11 +1,11 @@
 /*
  * Platform : BOJ (백준)
- * Problem  : 1181 단어 정렬
+ * Problem  : 25304 영수증
  * Tier     : Silver V
  * Link     : https://www.acmicpc.net/problem/1181
- * Idea     : 길이 → 사전순 정렬
- * Complex. : O(n log n)
- * Note     : 중복 제거
+ * Idea     : 
+ * Complex. : 
+ * Note     : 
  */
 
 #include <stdio.h>
