17bf6ece918e87e4d29ceff415c6d0c131390bfd
minseok <hamston@hs.ac.kr>
2025-10-01 13:31:08 +0900

    swea 홀수만더하기

---
+++ swea/D1/2072_홀수만_더하기/raw.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main(){
+    int t;
+    scanf("%d", &t);
+     
+    for (int i = 1 ; i <=  t ; i++){
+        int sum = 0;
+        int p = 0;
+        for (int T = 0 ; T < 10 ; T++){
+            scanf("%d", &p);
+            if (p % 2 != 0)
+                sum += p;
+        }
+        printf("#%d %d\n", i, sum);
+    }
+    return 0;
+}
