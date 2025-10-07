759b2b0d53a4001738c43161e47f02200bdee867
minseok <hamston@hs.ac.kr>
2025-10-07 14:34:41 +0900

    Create clean.c

---
+++ programmers/Level0/대소문자_바꿔서_출력하기/clean.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+#define LEN_INPUT 21
+
+int main(void) {
+    char s1[LEN_INPUT];
+    int n = 0;
+    
+    scanf("%s", s1);
+    
+    for (int i=0 ; s1[i] ; i++){
+        if(islower(s1[i])) s1[i] = toupper(s1[i]);
+        else s1[i] = tolower(s1[i]);
+    }
+    printf("%s", s1);
+    
+    return 0;
+}
