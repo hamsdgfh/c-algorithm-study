771d60404b2d589a5b0d538c86da70ccde339c0b
minseok <hamston@hs.ac.kr>
2025-10-10 21:24:09 +0900

    Create raw.c

---
+++ baekjoon/Bronze_III/10988_팰린드롬인지_확인하기/raw.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char word[101] = "";
+
+    scanf("%s", word);
+    int len = strlen(word)/2;
+
+    for(int i = 0 ; i < len ; i++){
+        if(word[i] != word[strlen(word) - 1 - i]){
+            printf("0");
+            return 0;
+        }
+    }
+    printf("1");
+    return 0;
+}
