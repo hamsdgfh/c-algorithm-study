6042d5ff8856dd1d423f988fef165697840f9202
minseok <hamston@hs.ac.kr>
2025-10-12 18:24:09 +0900

    Update raw.c

--- programmers/Level0/접두사인지_확인하기/raw.c
+++ programmers/Level0/접두사인지_확인하기/raw.c
@@ -1,3 +1,13 @@
+/*
+ * Platform : Programmers
+ * Level    : 0
+ * Problem  : 접두사인지 확인하기
+ * Link     : 
+ * Idea     : 
+ * Complex. : 
+ * Note     : 
+ */
+
 #include <stdio.h>
 #include <string.h>
 
