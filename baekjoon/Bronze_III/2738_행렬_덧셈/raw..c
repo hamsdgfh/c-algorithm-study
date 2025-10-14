b3c6f7724c80552c8740469fd4e917a0e7264bb0
minseok <hamston@hs.ac.kr>
2025-10-14 16:47:04 +0900

    Create raw..c

---
+++ baekjoon/Bronze_III/2738_행렬_덧셈/raw..c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int n; //행의 개수
+    int m; //열의 개수
+    int a[100][100] = {0};
+    int b[100][100] = {0};
+    int c[100][100] = {0};
+
+    scanf("%d%d", &n, &m);
+
+    for(int i=0 ; i<n ; i++){
+        for(int j=0 ; j<m ; j++){
+            scanf("%d", &a[i][j]);
+        }
+    }
+    for(int i=0 ; i<n ; i++){
+        for(int j=0 ; j<m ; j++){
+            scanf("%d", &b[i][j]);
+        }
+    }
+    for(int i=0 ; i<n ; i++){
+        for(int j=0 ; j<m ; j++){
+            c[i][j] = a[i][j] + b[i][j];
+        }
+    }
+    for(int i=0 ; i<n ; i++){
+        for(int j=0 ; j<m ; j++){
+            printf("%d ", c[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
