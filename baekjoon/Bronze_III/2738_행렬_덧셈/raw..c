d8e2699e2c1197feaad8e3ab1b8366a80108ea2f
minseok <hamston@hs.ac.kr>
2025-10-14 17:04:20 +0900

    Update raw..c

--- baekjoon/Bronze_III/2738_행렬_덧셈/raw..c
+++ baekjoon/Bronze_III/2738_행렬_덧셈/raw..c
@@ -32,3 +32,40 @@ int main() {
     }
     return 0;
 }
+
+
+//VAL 방법
+#include <stdio.h>
+
+int main() {
+    int n; //행의 개수
+    int m; //열의 개수
+
+    scanf("%d%d", &n, &m);
+    int a[n][m];
+    int b[n][m];
+    int c[n][m];
+    
+    for(int i=0 ; i<n ; i++){
+        for(int j=0 ; j<m ; j++){
+            scanf("%d", &a[i][j]);
+        }
+    }
+    for(int i=0 ; i<n ; i++){
+        for(int j=0 ; j<m ; j++){
+            scanf("%d", &b[i][j]);
+        }
+    }
+    for(int i=0 ; i<n ; i++){
+        for(int j=0 ; j<m ; j++){
+            c[i][j] = a[i][j] + b[i][j];
+        }
+    }
+    for(int i=0 ; i<n ; i++){
+        for(int j=0 ; j<m ; j++){
+            printf("%d ", c[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
