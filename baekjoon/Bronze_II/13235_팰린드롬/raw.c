63c51f044ed770386886eb04cbb0683a568b9b8a
minseok <hamston@hs.ac.kr>
2025-11-11 18:29:16 +0900

    Update raw.c

--- baekjoon/Bronze_II/13235_팰린드롬/raw.c
+++ baekjoon/Bronze_II/13235_팰린드롬/raw.c
@@ -12,6 +12,23 @@ int palindrome(char* s){
     return 1;
 }
 
+//오리지널 문자열을 뒤집어서, 동일한 문자열이면 1 아니면 0을 반환
+int palindrome2(char* s){
+    int n = strlen(s);
+    char* s2 = (char*)malloc(n+1);    //heap 영역에 저장 : 반드시 내가 메모리 해제
+
+    for(int i=0 ; i<n ; i++){
+        //s[i]를 s2[n-i-1]로 카피
+        s2[n-i-1] = s[i];
+    }
+    s2[n] = '\0';
+
+    int result = (strcmp(s,s2) == 0);
+
+    free(s2);
+    return result;
+}
+
 int main() {
     char word[21] = "";
 
